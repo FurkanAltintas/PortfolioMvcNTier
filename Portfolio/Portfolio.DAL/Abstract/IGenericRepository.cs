4fd38eeab7f3a71f9ae963a8f707e815abffc5f7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 21:46:35 +0300

    Generic Repository

---
+++ Portfolio/Portfolio.DAL/Abstract/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        List<T> List(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        T Get(int id);
+
+        void Add(T p);
+        void Update(T p);
+
+        void Delete(T p);
+        void Delete(int id);
+    }
+}
