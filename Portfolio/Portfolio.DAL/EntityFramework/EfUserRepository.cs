0353598a24fcb1fc7b2df8c181b7b8162be519d7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 22:03:25 +0300

    EfUserRepository

---
+++ Portfolio/Portfolio.DAL/EntityFramework/EfUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Portfolio.ENTITY.Concrete;
+using Portfolio.DAL.Abstract;
+using Portfolio.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.EntityFramework
+{
+    public class EfUserRepository : GenericRepository<User>, IUserDal
+    {
+    }
+}
