bf62687476318be8735d0b1be40e82820a832984
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 21:21:32 +0300

    EntityLayer - Work

--- Portfolio/EntityLayer/Concrete/Work.cs
+++ Portfolio/EntityLayer/Concrete/Work.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Work
+    public class Work : BaseEntity
     {
         [Key]
-        public int WorkId { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Desription { get; set; }
     }
