378afc578440c63c2b01b4ea62dbe76e4c2a96b9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 21:21:21 +0300

    EntityLayer - User

---
+++ Portfolio/EntityLayer/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class User : BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AuthorityId { get; set; }
+        [ForeignKey("AuthorityId")]
+        public Authority Authority { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public virtual string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
