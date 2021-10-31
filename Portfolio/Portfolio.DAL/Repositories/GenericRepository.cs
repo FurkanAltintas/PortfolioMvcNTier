2370c90a4a5ae3d3c1eb161e3d1d6064bd870da9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 21:46:49 +0300

    Generic Repository

---
+++ Portfolio/Portfolio.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using Portfolio.DAL.Abstract;
+using Portfolio.DAL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Add(T p)
+        {
+            using (var c = new Context())
+            {
+                c.Entry(p).State = EntityState.Added;
+                c.SaveChanges();
+            }
+            
+        }
+
+        public void Delete(T p)
+        {
+            using (var c = new Context())
+            {
+                c.Entry(p).State = EntityState.Deleted;
+                c.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var c = new Context())
+            {
+                Delete(c.Set<T>().Find(id));
+                c.SaveChanges();
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> filter = null)
+        {
+            using (var c= new Context())
+            {
+                return c.Set<T>().SingleOrDefault(filter);
+            }
+        }
+
+        public T Get(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Set<T>().Find(id);
+            }
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter = null)
+        {
+            using (var c = new Context())
+            {
+                return filter == null ?
+                    c.Set<T>().ToList() : c.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(T p)
+        {
+            using (var c = new Context())
+            {
+                c.Entry(p).State = EntityState.Modified;
+                c.SaveChanges();
+            }
+        }
+    }
+}
