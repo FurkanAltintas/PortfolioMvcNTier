9b460dc384e273f4a69770d8cbe870fed2d0821c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-31 21:27:15 +0300

    Context

---
+++ Portfolio/Portfolio.DAL/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Concrete
+{
+    public class Context : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Authority> Authorities { get; set; }
+        public DbSet<Background> Backgrounds { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Information> Informations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Work> Works { get; set; }
+    }
+}
