f2c84f11d5dd077cf577d695c4109d02b331a663
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-01 23:53:23 +0300

    Home Controller

---
+++ Portfolio/Portfolio.UI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Portfolio.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        Slider slider = new Slider();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(slider);
+        }
+    }
+}
