95e971c6d758b8d6ed79ce49c46f2fbb93fb15fa
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-01 23:54:54 +0300

    Generic Service

---
+++ Portfolio/Portfolio.BLL/Abstract/IGenericService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.BLL.Abstract
+{
+    public interface IGenericService<T>
+    {
+        List<T> List();
+
+        T Get(int id);
+
+        void Add(T p);
+
+        void Update(T p);
+
+        void Delete(T p, int id);
+    }
+}
