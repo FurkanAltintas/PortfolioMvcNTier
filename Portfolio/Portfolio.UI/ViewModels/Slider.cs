2d32be413081132b7ab1a2fdbbbc6122d817f8a6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-01 23:52:56 +0300

    ViewModels - Slider

---
+++ Portfolio/Portfolio.UI/ViewModels/Slider.cs
@@ -0,0 +1,14 @@
+﻿using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.UI.ViewModels
+{
+    public class Slider
+    {
+        public Background Background { get; set; }
+        public List<Degree> Degree { get; set; }
+    }
+}
