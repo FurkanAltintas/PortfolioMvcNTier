b47a7384082b00f1d2abe8a4444fd2aac313f077
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-03 17:59:46 +0300

    Contact Controller

---
+++ Portfolio/Portfolio.UI/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using Portfolio.BLL.Concrete;
+using Portfolio.DAL.EntityFramework;
+using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository(),
+                                                           new EfInformationRepository());
+        // GET: Contact
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Contact p)
+        {
+            p.IsActive = true;
+            p.CreateDate = DateTime.Now;
+            contactManager.Add(p);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult Address()
+        {
+            return PartialView(contactManager.GetAddress());
+        }
+    }
+}
