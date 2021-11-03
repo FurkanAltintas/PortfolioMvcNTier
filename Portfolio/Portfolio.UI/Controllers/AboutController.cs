d22ede7a5c001b2ed7bac99252f8c9549427cbe9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-03 17:59:57 +0300

    About Controller

---
+++ Portfolio/Portfolio.UI/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Portfolio.BLL.Concrete;
+using Portfolio.DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        // GET: About
+        public ActionResult Index()
+        {
+            return View(aboutManager.Get());
+        }
+    }
+}
