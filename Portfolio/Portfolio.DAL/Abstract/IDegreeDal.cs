e0699fd2159a82015eb6b5e88e36bc288fc54feb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-03 18:03:18 +0300

    Abstract - Degree

---
+++ Portfolio/Portfolio.DAL/Abstract/IDegreeDal.cs
@@ -0,0 +1,13 @@
+﻿using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Abstract
+{
+    public interface IDegreeDal : IGenericRepository<Degree>
+    {
+    }
+}
