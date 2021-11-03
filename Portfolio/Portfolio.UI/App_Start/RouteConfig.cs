d744357b1b5e86368277c2023703a9927c3fa3f0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-03 18:00:47 +0300

    Route Config

--- Portfolio/Portfolio.UI/App_Start/RouteConfig.cs
+++ Portfolio/Portfolio.UI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Portfolio.UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Detail",
+                url: "{controller}/{id}/{filter}",
+                defaults: new { controller = "Works", action = "Detail", id = UrlParameter.Optional, String = "" }
+);
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
