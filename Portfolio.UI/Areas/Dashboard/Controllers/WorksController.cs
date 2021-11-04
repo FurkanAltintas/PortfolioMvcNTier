1866bb8ae9c8d8166d8c70971e2f0a3cac5c0581
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-05 00:29:10 +0300

    Dashboard - Works

--- Portfolio.UI/Areas/Dashboard/Controllers/WorksController.cs
+++ Portfolio.UI/Areas/Dashboard/Controllers/WorksController.cs
@@ -25,12 +25,33 @@ namespace Portfolio.UI.Areas.Dashboard.Controllers
         }
 
         [HttpGet]
-        public ActionResult Create(Work p)
+        public ActionResult Edit(int id)
         {
-            p.IsActive = true;
-            p.IsActive = DateTime.Now;
-            workManager.Add(p);
+            return View(Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult Process(Work p)
+        {
+            if (p.Id > 0)
+            {
+                var key = Get(p.Id);
+                p.CreateDate = key.CreateDate;
+                p.IsActive = key.IsActive;
+                workManager.Update(p);
+            }
+            else
+            {
+                p.IsActive = true;
+                p.CreateDate = DateTime.Now;
+                workManager.Add(p);
+            }
             return RedirectToAction("Index");
+        }
+
+        public Work Get(int id)
+        {
+            return workManager.Get(id);
         }
     }
 }
