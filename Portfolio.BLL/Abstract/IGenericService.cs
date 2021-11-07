9d51e1abbb0e70436df8e7aeb75c9fba632f20fa
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-07 18:47:25 +0300

    Generic Service

--- Portfolio.BLL/Abstract/IGenericService.cs
+++ Portfolio.BLL/Abstract/IGenericService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,14 @@ namespace Portfolio.BLL.Abstract
 {
     public interface IGenericService<T>
     {
-        List<T> List();
+        List<T> List(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter = null);
 
         T Get(int id);
 
         void Add(T p);
-
         void Update(T p);
-
-        void Delete(T p, int id);
+        void Delete(int id);
+        void Delete(T p);
     }
 }
