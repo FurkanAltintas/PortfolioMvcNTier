db5ebd416a260d157f111c94e6cc6ab4e0b7e455
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-07 18:36:04 +0300

    Account Controller

---
+++ Portfolio.UI/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Portfolio.BLL.Concrete;
+using Portfolio.DAL.EntityFramework;
+using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Portfolio.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserManager userManager = new UserManager(new EfUserRepository());
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User p)
+        {
+            if (userManager.User(p.Email,p.Password))
+            {
+                FormsAuthentication.SetAuthCookie(p.Email, false);
+                Session["Mail"] = p.Email;
+                return RedirectToAction("Index", "Home", new { Area = "Dashboard" });
+            }
+            else
+            {
+                ViewBag.Error = "Mail veya şifreniz yanlış";
+                return View(p);
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return View("Login");
+        }
+    }
+}
