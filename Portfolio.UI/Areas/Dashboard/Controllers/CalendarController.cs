c753c955e7e21407d68cd6df96689fa965b49605
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 15:38:25 +0300

    Calendar Controller

---
+++ Portfolio.UI/Areas/Dashboard/Controllers/CalendarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.UI.Areas.Dashboard.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Dashboard/Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
