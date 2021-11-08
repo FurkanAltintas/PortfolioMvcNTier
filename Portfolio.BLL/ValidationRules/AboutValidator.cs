13c31f9986c656d014fdef049c43555516cbca9b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 15:37:46 +0300

    About Validator

---
+++ Portfolio.BLL/ValidationRules/AboutValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.BLL.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Başlık alanını boş bırakamazsınız")
+                               .MaximumLength(100).WithMessage("Başlık alanı 100 karakterden fazla olamaz");
+
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama alanını boş bırakamazsınız")
+                                     .MaximumLength(1000).WithMessage("Açıklama alanı 1000 karakterden fazla olamaz");
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Görsel alanını boş bırakamazsınız");
+        }
+    }
+}
