234b8648980ff55d7215a9146feca7a826ac9bd2
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 15:38:10 +0300

    Background Controller

---
+++ Portfolio.UI/Areas/Dashboard/Controllers/BackgroundController.cs
@@ -0,0 +1,30 @@
+﻿using Portfolio.BLL.Concrete;
+using Portfolio.DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.UI.Areas.Dashboard.Controllers
+{
+    public class BackgroundController : Controller
+    {
+        BackgroundManager backgroundManager = new BackgroundManager
+            (
+            new EfBackgroundRepository(),
+            new EfDegreeRepository()
+            );
+        // GET: Dashboard/Background
+        public ActionResult Index()
+        {
+            return View(backgroundManager.Get());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
