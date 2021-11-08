4fec2bae158743e1a4d6b8a3326677699effb264
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-11-08 15:37:55 +0300

    Degree Validator

---
+++ Portfolio.BLL/ValidationRules/DegreeValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Portfolio.ENTITY.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.BLL.ValidationRules
+{
+    public class DegreeValidator : AbstractValidator<Degree>
+    {
+        public DegreeValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanını boş bırakamazsınız")
+                              .MinimumLength(1).WithMessage("Ad alanı 1 karakterden fazla olması gerekmektedir")
+                              .MaximumLength(150).WithMessage("Ad alanı 150 karakterden fazla olması gerekmektedir");
+        }
+    }
+}
